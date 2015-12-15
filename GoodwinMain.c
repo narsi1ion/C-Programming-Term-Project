08e21d2d3b8b9bb85eff2c7f8127f13d0d495fae
Tessa <tfklettl@gmail.com>
2015-12-14 21:23:43 -0500

    IT WORKS HOLY SMACKEROONIERS

--- GoodwinMain.c
+++ GoodwinMain.c
@@ -4,16 +4,18 @@
 
 int main(){
 
-const int maxn;
+const int maxn = 500;
 int n;
 float h;    // h = timestep
-conc result[n];
+conc result[maxn];
 
-scanf( "%f%f%f%f", &result[0].X, &result[0].Y, &result[0].Z, &h );
+scanf( "%f %f %f %f", &result[0].X, &result[0].Y, &result[0].Z, &h );
 
 for(n=1;n<=maxn;n++){
     result[n] = RungeKutta(result[n-1],h);
+	printf("%f %f %f\n",result[n].X, result[n].Y, result[n].Z);
 }
+
 
 return 0;
 }
