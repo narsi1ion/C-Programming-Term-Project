cda721ec7115f30736daba991badd14818ce45ab
Tessa <tfklettl@gmail.com>
2015-12-15 13:50:47 -0500

    Added timestep input check in main program

--- GoodwinMain.c
+++ GoodwinMain.c
@@ -4,12 +4,16 @@
 
 int main(){
 
-const int maxn = 500;
+const int maxn = 10;
 int n;
 float h;    // h = timestep
 conc result[maxn];
 
 scanf( "%f %f %f %f", &result[0].X, &result[0].Y, &result[0].Z, &h );
+if(h<0){
+	printf("The timestep size %g is not valid.\n",h);
+	return (-1);
+}
 
 for(n=1;n<=maxn;n++){
     result[n] = RungeKutta(result[n-1],h);
