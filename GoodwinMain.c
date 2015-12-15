b4e5af152c6ed6e9585918a46e69c12607752520
Tessa <tfklettl@gmail.com>
2015-12-15 15:02:34 -0500

    Added checks for valid input for mRNA, enzyme & inhibitor initial conditions

--- GoodwinMain.c
+++ GoodwinMain.c
@@ -4,16 +4,29 @@
 
 int main(){
 
-const int maxn = 10;
+const int maxn = 500000;
 int n;
 float h;    // h = timestep
 conc result[maxn];
 
 scanf( "%f %f %f %f", &result[0].X, &result[0].Y, &result[0].Z, &h );
 if(h<0){
-	printf("The timestep size %g is not valid.\n",h);
+	printf("The timestep size %g is not valid, must be >0.\n",h);
 	return (-1);
 }
+if(result[0].X<0){
+	printf("The mRNA concentration %g is not valid, must be >0.\n",result[0].X);
+	return (-1);
+}
+if(result[0].Y<0){
+	printf("The enzyme concentration %g is not valid, must be >0.\n",result[0].Y);
+	return (-1);
+}
+if(result[0].Z<0){
+	printf("The inhibitor concentration %g is not valid, must be >0.\n",result[0].Z);
+	return (-1);
+}
+
 
 for(n=1;n<=maxn;n++){
     result[n] = RungeKutta(result[n-1],h);
